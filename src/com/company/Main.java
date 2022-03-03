ec8e8a37d31b9a9c93a3a98a42c1ae238777fb32
Rad <ggggg@MacBook-Pro-ggggg.local>
2022-03-04 02:10:24 +0400

    created program

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for(int i=0;i<20;i++)
+        System.out.println(Math.ceil(Math.random()*5));
+    }
+}
